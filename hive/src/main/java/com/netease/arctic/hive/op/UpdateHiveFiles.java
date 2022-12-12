b1e841f649691da431bd4f83b327f82260f46f88
shendanfeng01 <shendanfeng01@corp.netease.com>
2022-12-12 14:02:06 +0800

    fix hms failed lost data`

--- hive/src/main/java/com/netease/arctic/hive/op/UpdateHiveFiles.java
+++ hive/src/main/java/com/netease/arctic/hive/op/UpdateHiveFiles.java
@@ -113,11 +113,16 @@ public abstract class UpdateHiveFiles<T extends SnapshotUpdate<T>> implements Sn
       return;
     }
 
-    if (table.spec().isUnpartitioned()) {
-      commitNonPartitionedTable();
-    } else {
-      commitPartitionedTable();
+    try {
+      if (table.spec().isUnpartitioned()) {
+        commitNonPartitionedTable();
+      } else {
+        commitPartitionedTable();
+      }
+    } catch (Exception e) {
+      LOG.warn("Commit operation to HMS failed. ", e);
     }
+    
   }
 
   private void commitPartitionProperties() {
